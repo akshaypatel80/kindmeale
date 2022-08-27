2b633f31a26ab106d6c4f5d3804e66585b20aaab
aki9662804780 <akpatel260199@gmail.com>
2022-08-27 21:35:57 +0530

    Login

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import AuthContex from "./context/AuthContex";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <AuthContex>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+        </AuthContex>
     </BrowserRouter>
   </React.StrictMode>
 );
